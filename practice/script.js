d7a3855bf3e6219fdfc761881e319e80f52e19d4
Yusuke Fukumoto <yskf.recurrent01@gmail.com>
2026-04-01 10:48:53 +0900

    ソースコードを修正。コメントを追加

--- practice/script.js
+++ practice/script.js
@@ -1,2 +1,1 @@
-const areas = document.querySelectorAll('.area');
 const items = document.querySelectorAll('.item');
--- practice/script_finish.js
+++ practice/script_finish.js
@@ -1,16 +1,19 @@
-const areas = document.querySelectorAll('.area');
+// すべてのアイテムを取得
 const items = document.querySelectorAll('.item');
 items.forEach(item => {
+    // アイテムにイベントリスナーを登録
     item.addEventListener('click', e => {
+        // クリックされたアイテムが存在するエリア(親要素)のidを取得
         let currentAreaId = e.target.parentElement.id;
         let targetAreaId = '';
+        // 現在地と反対のエリアのidをセット
         if (currentAreaId === 'area-left') {
             targetAreaId = 'area-right';
         } else {
             targetAreaId = 'area-left';
         }
-
         const targetArea = document.getElementById(targetAreaId);
+        // アイテムを目的地へ移動させる
         targetArea.append(e.target);
     });
 });
