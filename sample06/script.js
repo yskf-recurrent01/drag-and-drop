c6e6ba8d682de771dfe79c8801b0931f7def6775
Yusuke Fukumoto <yskf.recurrent01@gmail.com>
2026-04-02 16:35:34 +0900

    ファイルプレビュー表示とファイルアップロード機能作成

--- sample06/script.js
+++ sample06/script.js
@@ -1,5 +1,9 @@
 const dropZone = document.getElementById('drop-zone');
 const previewArea = document.getElementById('preview');
+const uploadBtn = document.getElementById('upload-btn');
+
+const INVALID_FILE_TYPE = 1;
+const UPLOAD_FAILED = 2;
 
 ['dragover', 'dragleave', 'drop'].forEach(eventName => {
   dropZone.addEventListener(eventName, (e) => {
@@ -19,21 +23,75 @@ dropZone.addEventListener('dragleave', (e) => {
 dropZone.addEventListener('drop', (e) => {
   const files = e.dataTransfer.files;
   console.log(files);
-  if (handleFiles(files)) {
-    console.log('画像です')
-  } else {
-    console.log('画像じゃないです')
-  }
-
+  handleFiles(files);
   dropZone.classList.remove('drag-over');
 });
 
+
 function handleFiles(files) {
   const file = files[0];
-  console.log(file.type.match(/^image/));
+  if (!checkFileType(file)) {
+    showResultMessage({
+      status: 'error',
+      data: null,
+      error: {
+        code: INVALID_FILE_TYPE,
+      }
+    })
+  } else {
+    showPreview(file);
+    uploadBtn.addEventListener('click', () => {
+      uploadFile(file);
+    })
+  }
+}
 
-  if (!file.type.match(/^image/)) return false;
+function checkFileType(file) {
+  if (file.type.match(/^image/)) {
+    return true
+  } else {
+    return false;
+  }
+}
 
+function showResultMessage(json) {
+  const resuleArea = document.getElementById('result-area');
+  resuleArea.innerHTML = '';
+  let className = '';
+  let msg = '';
+  if (json.status === 'success') {
+    className = 'success';
+    msg = `ファイルのアップロードに成功しました:<br> [${json.data.filename}]`;
+  } else if (json.status === 'error') {
+    className = 'danger';
+    switch (json.error.code) {
+      case INVALID_FILE_TYPE:
+        msg = 'アップロードできるファイルの種類はJPEG,PNG,WEBPのみです';
+        break;
+      case UPLOAD_FAILED:
+        msg = 'アップロードに失敗しました';
+        break;
+    }
+  }
+
+  const html = `<div class="alert alert-${className} text-center"><p>${msg}</p><button type="button" class="btn-close" aria-label="Close" id="alert-close-btn"></button></div>`;
+  resuleArea.insertAdjacentHTML('beforeend', html);
+  previewArea.innerHTML = '';
+  document.getElementById('alert-close-btn').addEventListener('click', (e) => {
+    resuleArea.innerHTML = '';
+  });
+}
+
+function getResultMessage(json) {
+  switch (json) {
+    case INVALID_FILE_TYPE:
+      return 'アップロードできるファイルの種類はJPEG,PNG,WEBPのみです';
+    default:
+      return '';
+  }
+}
+
+function showPreview(file) {
   const render = new FileReader();
   render.onload = (e) => {
     const img = document.createElement('img');
@@ -44,18 +102,19 @@ function handleFiles(files) {
     // console.log(img);
   }
   render.readAsDataURL(file);
-
-  uploadFile(file);
-
-  return true;
+  uploadBtn.removeAttribute('disabled');
 }
 
-function uploadFile(file) {
+async function uploadFile(file) {
   const formData = new FormData();
   formData.append('image', file);
-  const res = fetch('./upload_file.php', {
+  const res = await fetch('./upload_file.php', {
     method: 'post',
     body: formData,
   });
-
+  const json = await res.json();
+  // console.log(json);
+  showResultMessage(json);
+  uploadBtn.disabled=true;
 }
+
--- sample06/upload_file.php
+++ sample06/upload_file.php
@@ -1,14 +1,16 @@
 <?php
 require_once 'functions.php';
 
-var_dump($_FILES);
+// var_dump($_FILES);
+define('INVALID_FILE_TYPE', 1);
+define('UPLOAD_FAILED', 2);
 
 $type = $_FILES['image']['type'];
 $name = $_FILES['image']['name'];
 $tmp_name = $_FILES['image']['tmp_name'];
 $type = $_FILES['image']['size'];
 
-$filepath_base = './images/';
+$filepath_base = 'images/';
 
 $allowed_image_types = [
   IMAGETYPE_JPEG,
@@ -17,14 +19,39 @@ $allowed_image_types = [
 ];
 
 $json = [
-  'msg' => '',
-  'filename' => '',
+  'status' => 'success',
+  'data' => null,
+  'error' => [
+    'code' => null,
+  ]
 ];
 
-if ($_FILES['image']['error'] !== 0) return;
-
-if (in_array(exif_imagetype($tmp_name), $allowed_image_types)) {
-  move_uploaded_file($tmp_name, $filepath_base . $name);
+if ($_FILES['image']['error'] === 0) {
+  if (in_array(exif_imagetype($tmp_name), $allowed_image_types)) {
+    $filename = $filepath_base . $name;
+    move_uploaded_file($tmp_name, $filename);
+    $json = [
+      'status' => 'success',
+      'data' => ['filename' => $filename],
+      'error' => null
+    ];
+  } else {
+    $json = [
+      'status' => 'error',
+      'data' => null,
+      'error' => [
+        'code' => INVALID_FILE_TYPE,
+      ],
+    ];
+  }
 } else {
-  $json['msg'] = 'アップロードできる画像の種類はJPEG,PNG,WEBPのみです';
+  $json = [
+    'status' => 'error',
+    'data' => null,
+    'error' => [
+      'code' => UPLOAD_FAILED,
+    ],
+  ];
 }
+echo json_encode($json, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+exit();
